f034c2a906b66dc1ba2f8aab789c832196aa1449
FlewRr <cool.adsdad@mail.ru>
2025-11-22 21:22:11 +0300

    config

--- moe_bert/pretrain.py
+++ moe_bert/pretrain.py
@@ -39,15 +39,24 @@ class WikiStreamDataset(IterableDataset):
 class BertMoEConfig(BertConfig):
     def __init__(
         self,
-        num_experts=4,
+        num_experts=1,
         expert_capacity=32,
         top_k=1,
+        router_bias=True,
+        dropout=0.0,
+        hidden_act="gelu",
+        layer_norm_eps=1e-12,
         **kwargs
     ):
         super().__init__(**kwargs)
         self.num_experts = num_experts
         self.expert_capacity = expert_capacity
         self.top_k = top_k
+        self.router_bias = router_bias
+        self.dropout = dropout
+        self.hidden_act = hidden_act
+        self.layer_norm_eps = layer_norm_eps
+
 
 # ----------------------------
 # Модель
